067e3309fc24a59f51c2cd3a39d4bca068cfb0af
André Fischer <andrefischer92@googlemail.com>
2016-12-16 23:48:57 +0100

    Fixed a bug in mail layer

--- layers/mail/init.el
+++ layers/mail/init.el
@@ -31,16 +31,16 @@
            (cons :format "%v"
                  (const :format "" auth-type)
                  (choice :tag "Auth-Type"
-                         (const login)
-                         (const clear)
-                         (const cram-md5)
-                         (const digest-md5)))
+                         (const 'login)
+                         (const 'clear)
+                         (const 'cram-md5)
+                         (const 'digest-md5)))
            (cons :format "%v"
                  (const :format "" conn-type)
                  (choice :tag "Conn-Type"
-                         (const clear)
-                         (const ssl)
-                         (const starttls)))))
+                         (const 'clear)
+                         (const 'ssl)
+                         (const 'starttls)))))
   :set (lambda (symbol value))
   :group 'gears-layers/mail)
 
@@ -69,19 +69,22 @@
            (cons :format "%v"
                  (const :format "" wl-smtp-authenticate-type)
                  (choice :tag "Auth-Type"
-                         (const login)
-                         (const clear)
-                         (const cram-md5)
-                         (const digest-md5)))
+                         (const 'login)
+                         (const 'clear)
+                         (const 'cram-md5)
+                         (const 'digest-md5)))
            (cons :format "%v"
                  (const :format "" wl-smtp-connection-type)
                  (choice :tag "Conn-Type"
-                         (const clear)
-                         (const ssl)
-                         (const starttls)))
+                         (const 'clear)
+                         (const 'ssl)
+                         (const 'starttls)))
            (cons :format "%v"
                  (const :format "" wl-local-domain)
-                 (string :tag "Domain (Mostly Server Domain)"))))
+                 (string :tag "Domain (Mostly Server Domain)"))
+           (cons :format "%v"
+                 (const :format "" wl-message-id-domain)
+                 (string :tag "Message Domain (Same as Server in most cases)"))))
   :group 'gears-layers/mail)
 
 (defcustom gears-layers/mail-default-smtp-account (car (car gears-layers/mail-smtp-accounts))
@@ -122,24 +125,47 @@
   (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
   (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
 
-  (setq elmo-imap4-use-modified-utf7 t
-        wl-template-alist gears-layers/mail-smtp-accounts
-        elmo-maildir-folder-path gears-layers/mail-folders-path
-        elmo-localdir-folder-path gears-layers/mail-folders-path
-        wl-stay-folder-window t
-        wl-folders-file gears-layers/mail-folders-file
-        wl-user-mail-address-list `,(mapcar #'(lambda (elem)
-                                                (cdr (assoc 'wl-local-domain (cdr elem))))
+  (let ((default-account-details
+          (cdr (assoc gears-layers/mail-default-smtp-account
+                      gears-layers/mail-smtp-accounts))))
+    (setq elmo-imap4-use-modified-utf7 t
+          wl-template-alist gears-layers/mail-smtp-accounts
+          elmo-maildir-folder-path gears-layers/mail-folders-path
+          elmo-localdir-folder-path gears-layers/mail-folders-path
+          wl-stay-folder-window t
+          wl-folders-file gears-layers/mail-folders-file
+          wl-user-mail-address-list (mapcar #'(lambda (elem)
+                                                ;; Parse wl-from to email-address
+                                                (let ((from (cdr (assoc 'wl-from (cdr elem)))))
+                                                  ;; "Remove < and >"
+                                                  (s-replace-all '(("<" . "") (">" . ""))
+                                                                 ;; Remove name, leave only email-address
+                                                                 (nth (- (length (split-string from)) 1) (split-string from)))))
                                             gears-layers/mail-smtp-accounts)
-        wl-default-folder gears-layers/mail-folders-path
-        wl-quicksearch-folder gears-layers/mail-folders-path
-        wl-draft-folder gears-layers/mail-draft-folder
-        wl-trash-folder gears-layers/mail-trash-folder
-        wl-queue-folder gears-layers/mail-queue-folder
-        wl-fcc-force-as-read t
-        wl-from (cdr (assoc 'wl-from
-                            (assoc gears-layers/mail-default-smtp-account
-                                   gears-layers/mail-smtp-accounts)))))
+          wl-default-folder gears-layers/mail-folders-path
+          wl-quicksearch-folder gears-layers/mail-folders-path
+          wl-draft-folder gears-layers/mail-draft-folder
+          wl-trash-folder gears-layers/mail-trash-folder
+          wl-queue-folder gears-layers/mail-queue-folder
+          wl-fcc-force-as-read t
+          wl-from (cdr (assoc 'wl-from
+                              (assoc gears-layers/mail-default-smtp-account
+                                     gears-layers/mail-smtp-accounts)))
+          wl-smtp-connection-type (cdr (assoc 'wl-smtp-connection-type
+                                              default-account-details))
+          wl-smtp-posting-port (cdr (assoc 'wl-smtp-posting-port
+                                           default-account-details))
+          wl-smtp-posting-server (cdr (assoc 'wl-smtp-posting-server
+                                             default-account-details))
+          wl-smtp-posting-user (cdr (assoc 'wl-smtp-posting-user
+                                           default-account-details))
+          wl-smtp-authentication-type (cdr (assoc 'wl-smtp-authentication-type
+                                                  default-account-details))
+          wl-local-domain (cdr (assoc 'wl-local-domain
+                                           default-account-details))
+          wl-smtp-message-id-domain (cdr (assoc 'wl-smtp-message-id-domain
+                                                default-account-details))
+          )))
 
 (defcustom gears-layers/mail-folders-file (s-concat gears-emacs-basepath
                                                     "/config/mail-folders.conf")
@@ -164,4 +190,4 @@
   t)
 
 (gears-layer-defdepends mail
-                        :packages '(wanderlust offlineimap))
+                        :packages '(wanderlust offlineimap smtpmail))
